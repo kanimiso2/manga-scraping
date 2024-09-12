ad1caf6086f2a0dcce93dd3aabbaec66fb3610fa
kanimiso2 <orungori@gmail.com>
2024-09-12 18:19:16 +0900

    Complete environment setup and verify API connection

---
+++ backend/db/seeds.rb
@@ -0,0 +1,26 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+Post.create!(
+    [
+      {
+        title: 'Lorem ipsum dolor sit amet.'
+      },
+      {
+        title: 'Id velit nesciunt et adipisci vitae aut eligendi sunt eum minima totam.'
+      },
+      {
+        title: 'Et cumque maiores eos voluptate tempore ut tempora dignissimos et soluta corrupti est nihil beatae.'
+      },
+      {
+        title: 'Et commodi quam non dolorem quia ea cumque distinctio qui exercitationem voluptate cum iusto distinctio vel modi pariatur et doloremque officiis.'
+      },
+      {
+        title: 'In saepe unde sed obcaecati similique quo beatae amet et Quis incidunt eos molestiae nobis et nihil tenetur?'
+      },
+    ]
+  )
