46720d68f6bc6f0f9cbd765c8a9671151a826c89
kanimiso2 <orungori@gmail.com>
2024-09-17 14:02:29 +0900

    fix openapi.yml

--- backend/app/controllers/api/v1/comics_controller.rb
+++ backend/app/controllers/api/v1/comics_controller.rb
@@ -3,27 +3,33 @@ class Api::V1::ComicsController < ApplicationController
     def index
         comics = Comic.all
         render json: comics, status: :ok
-      end
+    rescue StandardError => e
+        render json: { error: "Internal Server Error: #{e.message}" }, status: :internal_server_error
+    end
 
-      # GET /api/v1/comics/:id
-      def show
+    # GET /api/v1/comics/:id
+    def show
         comic = Comic.find(params[:id])
         render json: comic, status: :ok
-      rescue ActiveRecord::RecordNotFound
+    rescue ActiveRecord::RecordNotFound
         render json: { error: 'Comic not found' }, status: :not_found
-      end
+    rescue StandardError => e
+        render json: { error: "Internal Server Error: #{e.message}" }, status: :internal_server_error
+    end
 
-      # GET /api/v1/comics/search_by_from_scraping?from_scraping=XXXX
-      def search_by_from_scraping
+    # GET /api/v1/comics/search_by_from_scraping?from_scraping=XXXX
+    def search_by_from_scraping
         if params[:from_scraping].present?
-          comics = Comic.where(from_scraping: params[:from_scraping])
-          if comics.exists?
-            render json: comics, status: :ok
-          else
-            render json: { error: 'No comics found' }, status: :not_found
-          end
+            comics = Comic.where(from_scraping: params[:from_scraping])
+            if comics.exists?
+                render json: comics, status: :ok
+            else
+                render json: { error: 'No comics found' }, status: :not_found
+            end
         else
           render json: { error: 'from_scraping parameter is missing' }, status: :bad_request
         end
-      end
+    rescue StandardError => e
+        render json: { error: "Internal Server Error: #{e.message}" }, status: :internal_server_error
+    end
 end
